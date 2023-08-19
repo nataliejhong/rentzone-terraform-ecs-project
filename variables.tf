80f68c6f56044745099268ebfcc303629ca36ce6
Natalie Hong <nataliejhong@gmail.com>
2023-08-18 23:29:35 -0500

    created rest of project files

--- variables.tf
+++ variables.tf
@@ -49,3 +49,69 @@ variable "private_data_subnet_az2_cidr" {
     description = "private data subnet az2 cidr block"
     type        = string
 }
+
+# security groups variable
+variable "ssh_location" {
+    description = "ip address that can ssh into the servers"
+    type        = string
+}
+
+# rds variables
+variable "database_snapshot_identifier" {
+    description = "database snapshot name"
+    type        = string
+}
+
+variable "database_instance_class" {
+    description = "database instance type"
+    type        = string
+}
+
+variable "database_instance_identifier" {
+    description = "database instance identifier"
+    type        = string
+}
+
+variable "multi_az_deployment" {
+    description = "create a standby db instance"
+    type        = bool
+}
+
+# acm variables
+variable "domain_name" {
+    description = "domain name"
+    type        = string
+}
+
+variable "alternative_names" {
+    description = "sub domain name"
+    type        = string
+}
+
+# s3 variables
+variable "env_file_bucket_name" {
+    description = "s3 bucket name"
+    type        = string
+}
+
+variable "env_file_name" {
+    description = "env file name"
+    type        = string
+}
+
+# ecs variables
+variable "architecture" {
+    description = "ecs cpu architecture"
+    type        = string
+}
+
+variable "container_image" {
+    description = "container image uri"
+    type        = string
+}
+
+# route 53 variables
+variable "record_name" {
+    description = "sub domain name"
+    type        = string
+}
